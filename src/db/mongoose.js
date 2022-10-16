0f1f538afb87213a1eb8a2a06e224a4e4902eeb3
lkmreucllid <lkmreucllid01@gmail.com>
2022-10-16 14:01:45 +0530

    changed Mongodb

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
+// mongoose.connect(process.env.MONGODB_URL, {
+//     useNewUrlParser: true
+// })
+
+
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true
-})
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('Connected to mongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB..', err))
